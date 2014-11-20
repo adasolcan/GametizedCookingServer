f393a33a81494314663661df2c92999e648e661f
adasolcan <adasolcan@yahoo.com>
2014-11-20 15:35:30 +0200

    modified user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,64 +1,58 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :json
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    respond_with @users
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
+    respond_with @user
   end
 
   # GET /users/new
   def new
     @user = User.new
+    respond_with @user
   end
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+    respond_with @user
   end
 
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(params[:user])
+    @same_fb_id = User.find_by_fb_id(@user.fb_id)
+    if @same_fb_id == nil
+        @user.save
     end
+    respond_with @user
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+      @user = User.find(params[:id])
+      @user.update_attributes(params[:user])
+      respond_with @user
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with @user
   end
 
   private
