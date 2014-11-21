30856f91891e25bb4c3ff7cc8d0ebe3e9b30a4af
adasolcan <adasolcan@yahoo.com>
2014-11-21 15:58:49 +0200

    added recipes and categories tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :recipes
+
+  resources :categories
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119231949) do
+ActiveRecord::Schema.define(version: 20141121133406) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "message"
+    t.integer  "score"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "fb_id"
