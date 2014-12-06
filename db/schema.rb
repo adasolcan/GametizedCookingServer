5761e4dbc2884b41db41b418cd6acc8b48d04695
adasolcan <adasolcan@yahoo.com>
2014-12-06 22:27:31 +0200

    reset scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122212816) do
+ActiveRecord::Schema.define(version: 20141206201214) do
 
   create_table "badges", force: true do |t|
     t.string   "name"
