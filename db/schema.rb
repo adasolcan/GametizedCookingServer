183319a2675be14307ce5890a96653c6651e3331
adasolcan <adasolcan@yahoo.com>
2014-12-14 22:53:28 +0000

    added Plans table to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206201214) do
+ActiveRecord::Schema.define(version: 20141214224911) do
 
   create_table "badges", force: true do |t|
     t.string   "name"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20141206201214) do
   create_table "has_badges", force: true do |t|
     t.integer  "user_id"
     t.integer  "badge_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "plans", force: true do |t|
+    t.string   "user_id"
+    t.string   "name"
+    t.text     "plan_details"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
