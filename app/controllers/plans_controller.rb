bcbebb5a6bb039b2ba86912585a88c0ab6c3ac24
adasolcan <adasolcan@yahoo.com>
2014-12-21 20:28:17 +0200

    modified plans controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,10 +2,13 @@ class PlansController < ApplicationController
   protect_from_forgery :except => [:update, :delete, :create]
   before_action :set_plan, only: [:show, :edit, :update, :destroy]
 
+  respond_to :html, :json
+
   # GET /plans
   # GET /plans.json
   def index
     @plans = Plan.all
+    respond_with @plans
   end
 
   def show_by_user_id
@@ -17,55 +20,42 @@ class PlansController < ApplicationController
   # GET /plans/1
   # GET /plans/1.json
   def show
+    @plan = Plan.find(params[:id])
+    respond_with @plan
   end
 
   # GET /plans/new
   def new
     @plan = Plan.new
+    respond_with @plan
   end
 
   # GET /plans/1/edit
   def edit
+    @plan = Plan.find(params[:id])
+    respond_with @plan
   end
 
   # POST /plans
   # POST /plans.json
   def create
     @plan = Plan.new(plan_params)
-
-    respond_to do |format|
-      if @plan.save
-        format.html { redirect_to @plan, notice: 'Plan was successfully created.' }
-        format.json { render :show, status: :created, location: @plan }
-      else
-        format.html { render :new }
-        format.json { render json: @plan.errors, status: :unprocessable_entity }
-      end
-    end
+    @plan.save
+    respond_with @plan
   end
 
   # PATCH/PUT /plans/1
   # PATCH/PUT /plans/1.json
   def update
-    respond_to do |format|
-      if @plan.update(plan_params)
-        format.html { redirect_to @plan, notice: 'Plan was successfully updated.' }
-        format.json { render :show, status: :ok, location: @plan }
-      else
-        format.html { render :edit }
-        format.json { render json: @plan.errors, status: :unprocessable_entity }
-      end
-    end
+    @plan.update(plan_params)
+    respond_with @plan
   end
 
   # DELETE /plans/1
   # DELETE /plans/1.json
   def destroy
     @plan.destroy
-    respond_to do |format|
-      format.html { redirect_to plans_url, notice: 'Plan was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with @plan
   end
 
   private
