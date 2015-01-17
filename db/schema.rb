7ceaca5d52d02d012dc0282608284b4c5fb8c44c
adasolcan <adasolcan@yahoo.com>
2015-01-17 18:10:26 +0200

    added image_id column to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107224639) do
+ActiveRecord::Schema.define(version: 20150117154421) do
 
   create_table "badges", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150107224639) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_id"
   end
 
   create_table "checkins", force: true do |t|
